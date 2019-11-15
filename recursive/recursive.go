e0759e41769df6701084bcc5c667f4af4e5abed3
Kiet Tran <tuankiet1708@gmail.com>
2019-11-15 19:53:58 +0700

    recursive

--- recursive/recursive.go
+++ recursive/recursive.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func fact(n int) int {
@@ -14,7 +15,28 @@ func fact(n int) int {
 	return n * fact(n-1)
 }
 
+// func main() {
+// 	i, _ := strconv.Atoi(os.Args[1])
+// 	fmt.Println(fact(i))
+// }
+
 func main() {
-	i, _ := strconv.Atoi(os.Args[1])
-	fmt.Println(fact(i))
+	if len(os.Args) < 3 {
+		fmt.Println("Vui lòng thêm chuỗi và khóa: caesar <chuỗi> <khóa>")
+		return
+	}
+
+	k, _ := strconv.Atoi(os.Args[2])
+	caesar := func(r rune) rune {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			return 'A' + (r-'A'+rune(k))%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'+rune(k))%26
+		}
+		return r
+	}
+
+	fmt.Println(strings.Map(caesar, os.Args[1]))
+
 }
